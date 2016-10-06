b9c0a73d0f7e452b129ba56dfa1419031b9cc7cf
sruthiraopanchaneni <sruthiraopanchaneni@gmail.com>
2016-10-05 21:56:38 -0500

    status

--- src/main/java/src/main/courseapp/repositories/UserRepository.java
+++ src/main/java/src/main/courseapp/repositories/UserRepository.java
@@ -1,13 +1,11 @@
 package src.main.courseapp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import src.main.courseapp.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	@Query("select u from User u where u.userName=:userName")
 	User findByUserName(@Param("userName")String userName);
 
 
